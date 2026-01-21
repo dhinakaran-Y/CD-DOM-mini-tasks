211842969e49caffe371741d6435022a9f13d210
dhinakaran-Y <dhinakaran.y.dhina14@gmail.com>
2026-01-21 21:06:31 +0530

    modify : task-11 (live-character-changer) : textarea resize disabled , counterFn Space & Sentence added

--- src/js/11_live-character-counter.js
+++ src/js/11_live-character-counter.js
@@ -2,12 +2,21 @@ const textAreaEl = document.getElementById("textarea")
 const characterCountEL = document.getElementById("ch-count")
 const characterCountNoSpaceEL = document.getElementById("ch-count-no-space");
 const clearBtnEl = document.getElementById("clear-btn")
+const countSpaceEl = document.getElementById("count-space")
+const countSentenceEl = document.getElementById("count-sentence")
 
+// count
 textAreaEl.addEventListener("input", characterCountFn);
 textAreaEl.addEventListener("input", characterCountWithoutSpaceFn);
+textAreaEl.addEventListener("input", countSpaceFn)
+textAreaEl.addEventListener("input", countSentenceFn)
+
+// clear
 clearBtnEl.addEventListener("click", textClearFn)
 clearBtnEl.addEventListener("click", characterCountFn);
 clearBtnEl.addEventListener("click", characterCountWithoutSpaceFn);
+clearBtnEl.addEventListener("click", countSpaceFn);
+clearBtnEl.addEventListener("click", countSentenceFn);
 
 // character count include space
 function characterCountFn() {
@@ -24,6 +33,29 @@ function characterCountWithoutSpaceFn() {
     // console.log(noSpaceTextValArr);
 
     characterCountNoSpaceEL.textContent = noSpaceTextValArr.length 
+}
+
+// count space
+function countSpaceFn() {
+    const textVal = textAreaEl.value
+    const textValArr = textVal.split("")
+    
+    const spaceValArr = textValArr.filter(char => char === " ");
+    // console.log(noSpaceTextValArr);
+
+    countSpaceEl.textContent = spaceValArr.length
+}
+
+// count sentence
+function countSentenceFn() {
+    const textVal = textAreaEl.value
+    const sentenceValArr = textVal.trim().split(".")
+    
+    
+    const sentenceValArr2 = sentenceValArr.filter(char => char !== "");
+    // console.log(sentenceValArr2);
+
+    countSentenceEl.textContent = sentenceValArr2.length
 }
 
 //input text clear Fn
